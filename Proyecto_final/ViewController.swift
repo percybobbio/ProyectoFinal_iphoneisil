54784d5632450cdd9ce376ee5aa9ce403bd57eaf
PercyBobbio <percybobbio@gmail.com>
2020-10-13 19:41:39 -0500

    tercer commit

--- Proyecto_final/ViewController.swift
+++ Proyecto_final/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            // Do any additional setup after loading the view.
+                NotificationCenter.default.addObserver(self, selector: #selector(KeyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+                
+                NotificationCenter.default.addObserver(self, selector: #selector(self.KeyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+            }
 
-
-}
+        
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            NotificationCenter.default.removeObserver(self)
+        }
+        
+        
+               @objc func KeyboardWillShow(_ notification: Notification) {
+                   let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+                   let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+                
+                UIView.animate(withDuration: animationDuration){
+                    self.constraintBottomScroll.constant = keyboardFrame.height
+                    self.view.layoutIfNeeded()
+                }
+                  
+               }
+               @objc func KeyboardWillHide(_ notification: Notification){
+                    
+                      let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+                      let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+                   
+                   UIView.animate(withDuration: animationDuration){
+                       self.constraintBottomScroll.constant = 0
+                    self.view.layoutIfNeeded()
+                    
+                }
+            }
+    }
 
